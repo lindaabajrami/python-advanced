ad082d2f4d5ecc07cb3990eb98e09bacad09a30c
Student <lindaabajrami01@gmail.com>
2025-09-20 14:54:14 +0200

    modules packages

---
+++ main4.py
@@ -0,0 +1,7 @@
+from simple_math.math_utils import add_numbers, subtract_numbers
+
+sum = add_numbers(5,8)
+differenc=subtract_numbers(5,8)
+
+print(sum)
+print(differenc)
---
+++ simple_math/__init__.py
@@ -0,0 +1,7 @@
+from simple_math import add_numbers, subtract_numbers
+
+x = 10
+y = 5
+
+print("sum:", add_numbers(x, y))
+print("difference:", subtract_numbers(x, y))
